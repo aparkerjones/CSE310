4c51323a512dba8590497a06921af0f82b444c54
aparkerjones <parkor.j@gmail.com>
2026-04-09 10:52:29 -0600

    .

--- hello-world/obj/Debug/net10.0/hello-world.AssemblyInfo.cs
+++ hello-world/obj/Debug/net10.0/hello-world.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hello-world")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3eb929f1051ac26bfc46a2ea66224932af01ca7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bda96fa2c71e5559017cef5d901eae917059716d")]
 [assembly: System.Reflection.AssemblyProductAttribute("hello-world")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hello-world")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
